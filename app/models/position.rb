b2d5c204ce17483a0867f70cd5b5c0d543e1ec8a
DarthMax <mai11inj@studserv.uni-leipzig.de>
2013-04-08 20:00:35 +0200

    fixed setting of latitude longitude

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,9 +1,9 @@
 class Position < ActiveRecord::Base
-  attr_accessible :latitude, :longitude, :time, :track_id, :speed, :height, :angle, :created_at
+  attr_accessible :latitude, :longitude, :time, :track_id, :speed, :height, :angle
 
   default_scope :order => "time ASC"
 
-  belongs_to :track, :touch => true
+  belongs_to :track, :touch => true, :dependent => :destroy
   belongs_to :user
 
   validates_associated :track
@@ -19,6 +19,34 @@ class Position < ActiveRecord::Base
 
   after_save :update_track
 
+
+  def latitude= latitude
+    if latitude.class==String
+      if match= /\d+\.\d+([NSns])$/.match(latitude)
+        latitude.delete! match[1]
+        latitude=latitude.to_f
+
+        latitude *= -1 if match[1].downcase=='s'
+      end
+    end
+
+    write_attribute(:latitude,latitude)
+  end
+
+  def longitude= longitude
+    if longitude.class==String
+      if match= /\d+\.\d+([NSns])$/.match(longitude)
+        longitude.delete! match[1]
+        longitude=longitude.to_f
+
+        longitude *= -1 if match[1].downcase=='w'
+      end
+    end
+
+    write_attribute(:longitude,longitude)
+  end
+
+
   private
 
   def add_time
@@ -29,6 +57,8 @@ class Position < ActiveRecord::Base
   def update_track
     self.track.calculate_distance
   end
+
+
 
 
 end
